ecafabc8431c1782bcb6555c569f14dcc97dbeaa
4theKnowledge <tylerbikaun@gmail.com>
2023-07-13 15:40:49 +0800

    Added theming

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,23 @@ import Interface from "./features/Interface";
 import CreateGraph from "./features/CreateGraph";
 import { GraphProvider } from "./shared/context";
 import Home from "./features/Home";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
 
 function App() {
   return (
-    <GraphProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create" element={<CreateGraph />} />
-          <Route path="/:graphId" element={<Interface />} />
-          <Route path="*" element={<div>Page not found...</div>} />
-        </Routes>
-      </Router>
-    </GraphProvider>
+    <ThemeProvider theme={theme}>
+      <GraphProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<CreateGraph />} />
+            <Route path="/:graphId" element={<Interface />} />
+            <Route path="*" element={<div>Page not found...</div>} />
+          </Routes>
+        </Router>
+      </GraphProvider>
+    </ThemeProvider>
   );
 }
 
