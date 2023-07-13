62ec236ad074f334b1c7c7fefeeb76841e761e4a
4theKnowledge <tylerbikaun@gmail.com>
2023-07-13 15:41:11 +0800

    added constants

--- client/src/shared/constants.js
+++ client/src/shared/constants.js
@@ -1,3 +1,5 @@
+import { grey } from "@mui/material/colors";
+
 export const NODE_SIZE = 40;
 export const LINK_LENGTH_CONSTANT = 500;
 export const MAX_NODES = 500;
@@ -6,5 +8,9 @@ export const CANVAS_COLOR = "white";
 export const DEACTIVATE_COLOR = "lightgrey";
 export const DEACTIVATE_BORDER_COLOR = "red";
 export const REVIEWED_BORDER_COLOR = "#4caf50";
+export const SELECTED_BORDER_COLOR = "";
 
 export const DRAWER_WIDTH = 300;
+export const SECONDARY_DRAWER_WIDTH = 300;
+
+export const ICON_COLOR = grey[800];
