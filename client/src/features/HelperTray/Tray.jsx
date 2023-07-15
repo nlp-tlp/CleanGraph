2ac5e614512a04db651205cb18dbc9522dcbc0df
4theKnowledge <tylerbikaun@gmail.com>
2023-07-15 09:20:33 +0800

    added download button and handler.

--- client/src/features/HelperTray/Tray.jsx
+++ client/src/features/HelperTray/Tray.jsx
@@ -1,15 +1,26 @@
-import React, { useContext } from "react";
-import { Stack, IconButton, Tooltip } from "@mui/material";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import {
+  Stack,
+  IconButton,
+  Tooltip,
+  CircularProgress,
+  Box,
+} from "@mui/material";
+import { Link, useParams } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
 import HelpIcon from "@mui/icons-material/Help";
 import SettingsIcon from "@mui/icons-material/Settings";
 import CategoryIcon from "@mui/icons-material/Category";
+import DownloadForOfflineIcon from "@mui/icons-material/DownloadForOffline";
 import { GraphContext } from "../../shared/context";
 import { ICON_COLOR } from "../../shared/constants";
+import axios from "axios";
+import { saveAs } from "file-saver";
 
 const HelperTray = () => {
-  const [state, dispatch] = useContext(GraphContext);
+  const { graphId } = useParams();
+  const [dispatch] = useContext(GraphContext);
+  const [downloading, setDownloading] = useState(false);
   const handleIconClick = (viewOption) => {
     dispatch({ type: "TOGGLE_MODAL", payload: { view: viewOption } });
   };
@@ -32,6 +43,26 @@ const HelperTray = () => {
     },
   ];
 
+  const handleDownload = async () => {
+    try {
+      setDownloading(true);
+      const response = await axios.get(`/graph/download/${graphId}`, {
+        responseType: "blob",
+      });
+
+      if (response.status === 200) {
+        const blob = new Blob([response.data], { type: "application/json" });
+        saveAs(blob, `graph_${graphId}.json`);
+      } else {
+        throw new Error("Unable to download data");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setDownloading(false);
+    }
+  };
+
   return (
     <Stack direction="row" alignItems="center">
       {icons.map(({ icon, viewOption, title }, index) => (
@@ -45,6 +76,25 @@ const HelperTray = () => {
           </IconButton>
         </Tooltip>
       ))}
+      <Tooltip title="Click to download graph">
+        {downloading ? (
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            p="12px"
+          >
+            <CircularProgress size="1.25rem" />
+          </Box>
+        ) : (
+          <IconButton size="large" color="primary" onClick={handleDownload}>
+            <DownloadForOfflineIcon
+              fontSize="small"
+              sx={{ color: ICON_COLOR }}
+            />
+          </IconButton>
+        )}
+      </Tooltip>
       {/* <Tooltip title="Click to return to home"> */}
       <IconButton size="large" color="primary" component={Link} to="/">
         <HomeIcon fontSize="small" sx={{ color: ICON_COLOR }} />
