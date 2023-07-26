86eb41af98e86e068b96c55abf6ea0253cc05a3c
4theKnowledge <tylerbikaun@gmail.com>
2023-07-27 05:23:03 +0800

    Modified to skip plugins and handle EDM/CM plugins.

--- server/plugin_manager.py
+++ server/plugin_manager.py
@@ -1,24 +1,33 @@
 import os
 import importlib.util
 
-from plugin_interface import TextProcessorPluginInterface
+from plugin_interface import (
+    ErrorDetectionModelPluginInterface,
+    CompletionModelPluginInferface,
+)
 
 
 class PluginManager:
     def __init__(self):
-        self._plugins = {}
+        self._plugins = {"edm": {}, "cm": {}}
 
     def load_plugins(self, path):
         for filename in os.listdir(path):
             if filename.endswith(".py"):
                 module_name = filename[:-3]
-                spec = importlib.util.spec_from_file_location(
-                    module_name, f"{path}/{filename}"
-                )
-                module = importlib.util.module_from_spec(spec)
-                spec.loader.exec_module(module)
-                if issubclass(module.Plugin, TextProcessorPluginInterface):
-                    self._plugins[module_name] = module.Plugin()
+
+                if not module_name.startswith("_"):
+                    spec = importlib.util.spec_from_file_location(
+                        module_name, f"{path}/{filename}"
+                    )
+                    module = importlib.util.module_from_spec(spec)
+                    spec.loader.exec_module(module)
+
+                    if issubclass(module.Plugin, ErrorDetectionModelPluginInterface):
+                        self._plugins["edm"][module_name] = module.Plugin()
+
+                    if issubclass(module.Plugin, CompletionModelPluginInferface):
+                        self._plugins["cm"][module_name] = module.Plugin()
 
     def get_plugins(self):
         return self._plugins
