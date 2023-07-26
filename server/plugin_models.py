1dbbea7be4eeec147d4b7db371a57db6296537fa
4theKnowledge <tylerbikaun@gmail.com>
2023-07-27 05:22:38 +0800

    Extended and made data model more specific.

--- server/plugin_models.py
+++ server/plugin_models.py
@@ -1,9 +1,46 @@
+from typing import List, Union, Optional, Dict
 from pydantic import BaseModel
+from enum import Enum
+
+from models.graph import Triple, BaseSuggestion, BaseError
 
 
-class InputModel(BaseModel):
-    text: str
+class ModelTriple(Triple):
+    head_properties: Optional[List[Dict]]
+    head_id: str
+    relation_properties: Optional[List[Dict]]
+    relation_id: str
+    tail_properties: Optional[List[Dict]]
+    tail_id: str
 
 
-class OutputModel(BaseModel):
-    result: str
+class ModelInput(BaseModel):
+    triples: List[Triple]
+
+
+class Error(BaseError):
+    id: str
+    is_node: bool
+
+
+class Suggestion(BaseSuggestion):
+    id: str
+    is_node: bool
+
+
+class ErrorsOutput(List[Error]):
+    pass
+
+
+class SuggestionsOutput(List[Suggestion]):
+    pass
+
+
+class OutputType(str, Enum):
+    errors = "errors"
+    suggestions = "suggestions"
+
+
+class ModelOutput(BaseModel):
+    type: OutputType
+    data: Union[ErrorsOutput, SuggestionsOutput]
