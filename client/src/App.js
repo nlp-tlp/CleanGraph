d3c479c8c35a25c4c14ccb986660c9c82092f56c
4theKnowledge <tylerbikaun@gmail.com>
2023-07-27 05:19:23 +0800

    Refactored. Added page titles and snackbar provider.

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,116 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+  useParams,
+} from "react-router-dom";
 import Interface from "./features/Interface";
 import CreateGraph from "./features/CreateGraph";
-import { GraphProvider } from "./shared/context";
+import { GraphContext, GraphProvider } from "./shared/context";
 import Home from "./features/Home";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+import { SnackbarProvider } from "./shared/snackbarContext";
+import Landing from "./features/Landing";
+import { useContext, useEffect } from "react";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GraphProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/create" element={<CreateGraph />} />
-            <Route path="/:graphId" element={<Interface />} />
-            <Route path="*" element={<div>Page not found...</div>} />
-          </Routes>
-        </Router>
+        <SnackbarProvider>
+          <Components />
+        </SnackbarProvider>
       </GraphProvider>
     </ThemeProvider>
   );
 }
+
+// Title mapping for different routes
+const PAGESUFFIX = " | CleanGraph";
+const TITLES = {
+  "/": "Interactive KG Quality Assurance" + PAGESUFFIX,
+  "/home": "Home" + PAGESUFFIX,
+  "/create": "Create Graph" + PAGESUFFIX,
+};
+
+// Custom hook for setting the document title
+function useDocumentTitle() {
+  const location = useLocation();
+  const { graphId } = useParams();
+  const [state] = useContext(GraphContext);
+
+  useEffect(() => {
+    // Get title from mapping or use default
+    const title =
+      TITLES[location.pathname] ||
+      `${state.graph.name === "" ? "Loading..." : state.graph.name}` +
+        PAGESUFFIX ||
+      "Page Not Found";
+
+    // Set document title
+    document.title = title;
+  }, [location, graphId]);
+}
+
+// This component will be a wrapper for all your routes
+const PageWrapper = ({ children }) => {
+  useDocumentTitle();
+
+  return children;
+};
+
+const Components = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PageWrapper>
+                <Landing />
+              </PageWrapper>
+            }
+          />
+          <Route
+            path="/home"
+            element={
+              <PageWrapper>
+                <Home />
+              </PageWrapper>
+            }
+          />
+          <Route
+            path="/create"
+            element={
+              <PageWrapper>
+                <CreateGraph />
+              </PageWrapper>
+            }
+          />
+          <Route
+            path="/:graphId"
+            element={
+              <PageWrapper>
+                <Interface />
+              </PageWrapper>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <PageWrapper>
+                <div>Page not found...</div>
+              </PageWrapper>
+            }
+          />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
 export default App;
