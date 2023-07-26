96b17589d4de9d01d46b24b8d6d68ddff3b27091
4theKnowledge <tylerbikaun@gmail.com>
2023-07-27 05:17:53 +0800

    Removed deprecated constants.

--- client/src/shared/constants.js
+++ client/src/shared/constants.js
@@ -1,16 +1,16 @@
 import { grey } from "@mui/material/colors";
 
+export const NODE_RADII = { small: 30, medium: 40, large: 50 };
+export const NODE_RADIUS = 40;
 export const NODE_SIZE = 40;
 export const LINK_LENGTH_CONSTANT = 500;
 export const MAX_NODES = 500;
-export const HIGHLIGHT_ALPHA = 0.0;
 export const CANVAS_COLOR = "white";
-export const DEACTIVATE_COLOR = "lightgrey";
-export const DEACTIVATE_BORDER_COLOR = "red";
-export const REVIEWED_BORDER_COLOR = "#4caf50";
-export const SELECTED_BORDER_COLOR = "";
 
 export const DRAWER_WIDTH = 300;
 export const SECONDARY_DRAWER_WIDTH = 300;
 
 export const ICON_COLOR = grey[800];
+
+// Level4 is for snackbars to show above dialog/modals.
+export const ZIndexes = { level1: 1, level2: 2, level3: 3, level4: 10000 };
